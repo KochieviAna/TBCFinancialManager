8f221ae41a7a2835598a9791c80f1fde2cfe0c6c
KochieviAna <anna.kochievi@gmail.com>
2025-02-22 01:54:35 +0400

    Initial commit.

---
+++ TBCFinancialManager/App/TBCFinancialManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TBCFinancialManagerApp.swift
+//  TBCFinancialManager
+//
+//  Created by MacBook on 22.02.25.
+//
+
+import SwiftUI
+
+@main
+struct TBCFinancialManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
